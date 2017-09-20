a3b58706f41a4850841a7d8667db2127f682a2fc
leejoonhaeng <joonhaenglee.korea@gmail.com>
2017-09-20 21:20:50 +0900

    9/20 update
    here is my update

--- sketch2.js
+++ sketch2.js
@@ -3,6 +3,9 @@ var bgcolor;
 var canvas ; 
 var x = 100;
 var y =100;
+var slider; 
+var input;
+var nameP;
 
 var h1;
 
@@ -11,15 +14,21 @@ function setup() {
   //canvas.position(500,500);
   bgcolor = color(200);
   h1 = createP('my favorit subject is math');
-  createP(" ");
+  nameP  = createP("your name");
+ 
+
   button = createButton("click me");
 
   button.mousePressed(changeColor);
-  createSlider(0,10,5);
+  slider  = createSlider(0,100,50);
+  input = createInput('type your name');
 
   //createSlider(10,100,47);
 }
+function updateName(){
+	name = input.value();
 
+}
 
 function changeColor(){
 	bgcolor = color(random(255));
@@ -30,8 +39,9 @@ function draw() {
   
   background(bgcolor);
   fill(255,0,0);
-  ellipse(100+x,100+y,100,100);
-  h1.position(x,y);
+  ellipse(100,100,slider.value(), slider.value());
+  text(input.value(),100,10);
+  nameP.html(input.value());
 
   
 }
