426591ae3b2b65e69f5b4c3513012a0f981781dc
leejoonhaeng <joonhaenglee.korea@gmail.com>
2017-09-20 20:41:10 +0900

    update

--- sketch2.js
+++ sketch2.js
@@ -1,19 +1,37 @@
 var button;
 var bgcolor;
+var canvas ; 
+var x = 100;
+var y =100;
+
+var h1;
 
 function setup() {
-  createCanvas(300,300);
+  canvas = createCanvas(300,300);
+  //canvas.position(500,500);
   bgcolor = color(200);
-  createP('');
-  button = createButton("go");
-  button.mousePressed(changeColor);
+  h1 = createP('my favorit subject is math');
+  createP(" ");
+  button = createButton("click me");
 
-  createSlider(10,100,47);
+  button.mousePressed(changeColor);
+  createSlider(0,10,5);
+
+  //createSlider(10,100,47);
+}
+
+
+function changeColor(){
+	bgcolor = color(random(255));
 
 }
 
 function draw() {
-  background(0);
-  ellipse(mouseX,mouseY,100,100);
+  
+  background(bgcolor);
+  fill(255,0,0);
+  ellipse(100+x,100+y,100,100);
+  h1.position(x,y);
+
   
 }
