f7202ebf32e4208fecf730febde3037c82fc8536
leejoonhaeng <joonhaenglee.korea@gmail.com>
2017-09-22 01:16:50 +0900

    update all
    .

--- sketch2.js
+++ sketch2.js
@@ -3,27 +3,45 @@ var bgcolor;
 var canvas ; 
 var x = 100;
 var y =100;
+
 var sliderH;
+var h;
 var sliderA;
+var a;
 var sliderAL;
+var al;
 var sliderK; 
+var k;
+var sliderHN;
+var hn;
+var sliderVN; 
+var vn;
+
 var input;
 var nameP;
 var h1;
 
+
+
+
 function setup() {
-  canvas = createCanvas(300,300);
+  canvas = createCanvas(500,500,WEBGL);
   //canvas.position(500,500);
   bgcolor = color(200);
-  h1 = createP('my favorit subject is math');
+  h1 = createP('select value');
   createP("h Value");
   sliderH  = createSlider(0,100,50);
   createP("a Value");
   sliderA  = createSlider(0,100,20);
   createP("alpha Value");
-  sliderAL  = createSlider(1,60,20);
+  sliderAL  = createSlider(1,60,60);
   createP("k value");
-  sliderK  = createSlider(0,100,50);
+  sliderK  = createSlider(0,90,0);
+
+  createP("horizontal number");
+  sliderHN  = createSlider(2,20,5);
+  createP("vertical number");
+  sliderVN  = createSlider(2,20,5);
 
   createP(" ");
   button = createButton("update");
@@ -34,7 +52,6 @@ function setup() {
 }
 function UpdateName(){
 	name = input.value();
-
 }
 
 function changeColor(){
@@ -45,10 +62,56 @@ function changeColor(){
 function draw() {
   
   background(bgcolor);
-  fill(255,0,0);
-  ellipse(100,100,sliderH.value(), sliderH.value());
-  text("h = "+sliderH.value() + "\na = " + sliderA.value()+"\nalpha = "+sliderAL.value()+"\nk = "+sliderK.value(),100,10);
-  //nameP.html(input.value());
+  h = sliderH.value();
+  a = sliderA.value();
+  al = PI*(sliderAL.value())/180;
+  k = sliderK.value()/100;
+  hn = sliderHN.value();
+  vn = sliderVN.value();
+  rotateX(PI/2);
+  //rotateY(PI/3);
+
+
+//translateX(j=1, i = hn-1 )
+//translateY(j=vn, i = 0 )
+
+  for ( var j = 0; j <vn+1 ; j++){
+    for (var i = 0; i <hn ; i++){
+      push();
+      translate(-0.5*((h/(cos(al*k)*tan(al)))*(1%2)+a*cos(al*k)*(hn)),0,-0.5*(h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*vn);
+      beginShape();
+        stroke(20);
+        vertex((h/(cos(al*k)*tan(al)))*(j%2)+a*cos(al*k)*i ,a*sin(al*k)*(i%2),(h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*j);
+        vertex((h/(cos(al*k)*tan(al)))*(j%2)+a*cos(al*k)*(i+1) ,a*sin(al*k)*((i+1)%2),(h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*j);
+      endShape();
+      pop();
+    }
+}
+  for ( var j = 0; j <vn ; j++){
+    for (var i = 0; i <hn+1 ; i++){
+      push();
+      translate(-0.5*((h/(cos(al*k)*tan(al)))*(1%2)+a*cos(al*k)*(hn)),0,-0.5*(h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*vn);
+      beginShape();
+        stroke(20);
+        vertex((h/(cos(al*k)*tan(al)))*(j%2)+a*cos(al*k)*i ,a*sin(al*k)*(i%2),(h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*j);
+        vertex((h/(cos(al*k)*tan(al)))*((j+1)%2)+a*cos(al*k)*i ,a*sin(al*k)*(i%2),(h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*(j+1));
+      endShape();
+      pop();
+
+
+
+
+
+    }
+  }
+
+
+
+
+
+
+
+  (h/sin(al))*sqrt( 1- (cos(al)*cos(al))/(cos(al*k)*cos(al*k)))*j
 
   
 }
